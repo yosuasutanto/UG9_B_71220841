0cb5e7fe792afe9146aa4b6f581106291358d33f
yosuasutanto <118423136+yosuasutanto@users.noreply.github.com>
2022-11-17 13:00:27 +0700

    Add files via upload

---
+++ 1_B_71220841.py
@@ -0,0 +1,53 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:/Users/T U F/Documents/1_B_71220841.py ==============
+======= ALAT PENGHITUNG LINGKARAN SENTOT =======
+masukkan  phi = 3.14
+Traceback (most recent call last):
+  File "C:/Users/T U F/Documents/1_B_71220841.py", line 2, in <module>
+    a = int (input ("masukkan  phi = "))
+ValueError: invalid literal for int() with base 10: '3.14'
+
+=============== RESTART: C:/Users/T U F/Documents/1_B_71220841.py ==============
+======= ALAT PENGHITUNG LINGKARAN SENTOT =======
+masukkan  phi = 22/7
+Traceback (most recent call last):
+  File "C:/Users/T U F/Documents/1_B_71220841.py", line 2, in <module>
+    a = int (input ("masukkan  phi = "))
+ValueError: invalid literal for int() with base 10: '22/7'
+>>> 
+=============== RESTART: C:/Users/T U F/Documents/1_B_71220841.py ==============
+======= ALAT PENGHITUNG LINGKARAN SENTOT =======
+masukkan  phi = 22/7
+Traceback (most recent call last):
+  File "C:/Users/T U F/Documents/1_B_71220841.py", line 2, in <module>
+    a = int (input ("masukkan  phi = "))
+ValueError: invalid literal for int() with base 10: '22/7'
+>>> input
+<built-in function input>
+>>> print
+<built-in function print>
+>>> 
+=============== RESTART: C:/Users/T U F/Documents/1_B_71220841.py ==============
+======= ALAT PENGHITUNG LINGKARAN SENTOT =======
+masukkan  phi = 22/7
+Traceback (most recent call last):
+  File "C:/Users/T U F/Documents/1_B_71220841.py", line 2, in <module>
+    a = int (input ("masukkan  phi = "))
+ValueError: invalid literal for int() with base 10: '22/7'
+>>> 
+=============== RESTART: C:/Users/T U F/Documents/1_B_71220841.py ==============
+======= ALAT PENGHITUNG LINGKARAN SENTOT =======
+masukkan  phi = 3.14
+Traceback (most recent call last):
+  File "C:/Users/T U F/Documents/1_B_71220841.py", line 2, in <module>
+    a = int (input ("masukkan  phi = "))
+ValueError: invalid literal for int() with base 10: '3.14'
+>>> 
+=============== RESTART: C:/Users/T U F/Documents/1_B_71220841.py ==============
+======= ALAT PENGHITUNG LINGKARAN SENTOT =======
+masukkan  phi = 3.14
+masukkan jari-jari (meter) = 100
+ masukan Diameter (meter) : 200
+ jarak yang harus ditempuh: 31400.0
---
+++ 2_B_71220841.py
@@ -0,0 +1,3 @@
+print ("=========== INI PROGRAM SEDER HANA=========")
+a = int(input(" Masukkan Angka yang ingin di pangkatkan :"))
+print ("Angka anda telah di pangkat kan 7! anda mendapat angka :" , a**7)
