10fb68ddde2274d7657fe339605ceedb440e5063
yosuasutanto <118423136+yosuasutanto@users.noreply.github.com>
2022-11-17 13:01:28 +0700

    Add files via upload

---
+++ 3_b_71220841.py
@@ -0,0 +1,12 @@
+print ("========== CATATAN BELANJA =======")
+print ("========= Daftar 1 =========")
+print (" Daftar Belanja 1: ['sikat gigi', 'odol', 'shampoo', 'Sabun', 'ciduk']
+print ("========= Daftar 2 =========")
+print (" Daftar Belanja 2: ['Teh', 'gula', 'Garam','Micin', 'kecap']
+
+print ("jawab dengan angka [1/2")
+print('1. rubah belanjaan')
+print (' 2. keluar')
+a= int(input(" masukkan pilihan :"))
+b= int(input(" masukkan nama item daftar 1 :"))
+c= int(input(" masukkan index yang ingin dirubah:"))
